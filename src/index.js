b94383fb4ee763f1ad3d25194ac51a1c754aa453
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-24 10:30:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+function addTodo(event){
+  event.preventDefault();
+  const input = document.getElementById("new-task-description");
+  const text = input.value;
+  const list = document.getElementById('tasks');
+
+  const btn = document.createElement("button");
+  btn.textContent = "X";
+  btn.id= list.children.length;
+  btn.onclick = () => removeTodo(btn.id);
+
+  const li = document.createElement("li");
+  li.textContent = text.trim();
+  li.append(btn);
+
+  li.id= "todo"+list.children.length
+  list.append(li);
+  input.value = "" ;
+}
+
+function removeTodo(id){
+  console.log(`li#todo${id}`);
+  document.querySelector(`li#todo${id}`).remove();
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form= document.getElementById("create-task-form");
+  form.addEventListener("submit",addTodo);
+
 });
